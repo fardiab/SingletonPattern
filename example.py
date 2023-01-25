ef5018dd8e45bff194ddb92ad0db33b355333cff
fardiab <feridabd37@gmail.com>
2023-01-26 00:48:33 +0400

    adding maps

---
+++ example.py
@@ -0,0 +1,16 @@
+class hero:
+    # __instance = None
+    # def __new__(cls, *args, **kwargs):
+    #     if not cls.__instance:
+    #         cls.__instance = super(hero, cls).__new__(cls, *args, **kwargs)
+    #     return cls.__instance
+
+    def __init__(self, name, hp, mp):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+
+    def show(self):
+        print(self.name, self.hp, self.mp)
+
+h1 = hero('Radud', 100, 100)
